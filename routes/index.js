d76675668d2ef0617d29da3e279891ab4def7bdc
Simon Gabyshev <gabyshev.sa@gmail.com>
2021-06-24 18:46:32 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (!res.locals.broken) {
-    res.render('index', { title: 'Express' });
-  } else {
-  	res.status(500).send('Something broke!');
-  }
+ 	res.status(500).send('Something broke!');
 });
 
 module.exports = router;
